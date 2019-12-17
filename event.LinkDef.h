ab705cc7bad82961f028c3bf95e3ff6cb8720bc0
PierPaolo Savina <psavina@ui7-auger.cr.cnaf.infn.it>
2019-12-17 16:34:23 +0100

    modified LinkDef

--- event.LinkDef.h
+++ event.LinkDef.h
@@ -1,6 +1,10 @@
-#ifdef __CLING__
+#ifdef __CINT__
 
-#pragma link C** namespace myEvt
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedef;
 #pragma link C++ class myEvt::event+;
 
 #endif
